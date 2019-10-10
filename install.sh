8b92ba4690c536cc502c6873f7c8723691d5047f
Jongwhan Lee <jonghwan@crypto.com>
2019-10-10 18:08:52 +0800

    compile kcov

--- install.sh
+++ install.sh
@@ -22,3 +22,18 @@ if test -f "$FILE"; then
     make install
     cd ..
 fi
+
+FILE2=/usr/local/bin/kcov
+if test -f "$FILE2"; then
+    echo "$FILE exist"
+    wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+    tar xzf master.tar.gz
+    cd kcov-master
+    mkdir build
+    cd build
+    cmake ..
+    make
+    sudo make install
+    cd ../..
+    rm -rf kcov-master
+fi

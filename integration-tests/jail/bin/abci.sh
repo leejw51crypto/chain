6403a41ad1288142bf0183ad733675d2820c5262
jongwhan lee <leejw51@gmail.com>
2019-11-16 21:38:24 +0800

    add scripts

---
+++ integration-tests/jail/bin/abci.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./chain-abci --host 0.0.0.0 --port 26658 --chain_id test-ab  --genesis_app_hash   A6EEE423BB7FBB55A4DA98BF72CFFC321E669BEF085C4B67D9912F85180E2283   --enclave_server tcp://127.0.0.1:25933

7175fa2c562c9accc0a75d9a209cbcd9f6bb5a1d
jongwhan lee <leejw51@gmail.com>
2019-11-18 00:34:38 +0800

    add jail test

---
+++ integration-tests/jail/disk/jail_test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+print("jail & unjail test");
--- integration-tests/jail/run.sh
+++ integration-tests/jail/run.sh
@@ -4,7 +4,7 @@ echo "docker compose ok"
 #echo "wait for docker setting up"
 #sleep 1800
 #echo "done"
-#python3 ./disk/test.py
+python3 ./disk/jail_test.py
 #echo "test finished"
 #sleep 4
 #docker-compose down

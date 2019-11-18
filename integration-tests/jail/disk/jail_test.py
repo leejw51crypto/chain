051ae7b1422caf2e36d49ddf0766089e150cb839
jongwhan lee <leejw51@gmail.com>
2019-11-18 16:29:37 +0800

    show container

--- integration-tests/jail/disk/jail_test.py
+++ integration-tests/jail/disk/jail_test.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python3
 import docker
-client = docker.from_env()
-containers= client.containers.list()
-for container in containers:
-    id = container
-    print(id.id, id.name)
+import json
+import requests
+import datetime
+import time
+
+def show_containers() :
+	client = docker.from_env()
+	containers= client.containers.list()
+	for container in containers:
+	    id = container
+	    print(id.id, id.name)
+
+#show_containers()
+# tendermint rpc
+
+server="http://localhost:26657"
+
+def check_validators() :
+	try: 
+		x= requests.get("http://localhost:26657/validators")
+		data =len(x.json()["result"]["validators"])
+		print(data)
+		return data
+	except requests.ConnectionError:
+ 		return 0
+	except:
+		assert False
+
+def wait_for_ready() :
+	while True:
+		validators=check_validators()
+		if 2<= validators :
+			print("validators ready")
+			break
+		print("validators =", validators)
+		time.sleep(1)
+
+
+############################################################################3
+wait_for_ready()
+show_containers()

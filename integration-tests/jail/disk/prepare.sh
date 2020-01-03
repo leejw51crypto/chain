bf24b85e9b3d554f56ec6c6e99763ec9dfdad50b
jongwhan lee <jonghwan@crypto.com>
2020-01-03 15:07:03 +0800

    add spid, ias_api_key

--- integration-tests/jail/disk/prepare.sh
+++ integration-tests/jail/disk/prepare.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export SGX_MODE=HW
 export NETWORK_ID=AB
+export SPID=4F883E85867521D3B8CD9F6DFB7F4FB0
+export IAS_API_KEY=d120972e39fb484e8519f0347bda6973
 export RUSTFLAGS=-Ctarget-feature=+aes,+ssse3
 export PATH=$HOME/.cargo/bin:$HOME/bin:$PATH
 export APP_PORT=25933

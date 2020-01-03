664d1afde8eaa617044b4d67bee9e3e3942e19ea
jongwhan lee <jonghwan@crypto.com>
2020-01-03 16:54:32 +0800

    send amount works

--- integration-tests/bot/join_test.py
+++ integration-tests/bot/join_test.py
@@ -14,10 +14,14 @@ class Program :
         self.blockchain = Blockchain()
         # wallet a
         self.node0_address = ""
+        self.node0_address1 = ""
+        self.node0_transfer_address = ""
         self.node0_mnemonics= ""
 
         # wallet b
         self.node1_address = ""
+        self.node1_address1= ""
+        self.node1_transfer_address = ""
         self.node1_mnemonics=""
 
         # wallet b
@@ -52,6 +56,12 @@ class Program :
 
     def create_staking_address(self,name, passphrase):
         return self.rpc.address.create(name,'staking')
+
+    def activate_sync(self):
+        print("activate sync")
+        self.wallet.sync_unlock("a")
+        self.wallet.sync_unlock("b")
+        self.wallet.sync_unlock("c")
        
     def restore_wallets(self):
         print("restore wallets")
@@ -155,21 +165,38 @@ class Program :
         print("unjail test success")
 
     ############################################################################3
-    def main (self) :
+    def main2 (self) :
         self.test_jailing()
         try :
             self.restore_wallets()
+            self.activate_sync()
         except jsonrpcclient.exceptions.JsonRpcClientError as ex:
             print("wallet already exists={}".format(ex))
         self.create_addresses()
         self.test_unjailing()
 
-    def main2 (self) :
+    def prepare(self) :
         try :
             self.restore_wallets()
         except jsonrpcclient.exceptions.JsonRpcClientError as ex:
             print("wallet already exists={}".format(ex))
         self.create_addresses()
+        self.rpc.staking.withdraw_all_unbonded(self.node0_address1, self.node0_transfer_address,[], "a")
+        self.rpc.wallet.sync_unlock("a")
+        self.rpc.wallet.sync("a")
+ 
+    def main (self) :
+        #self.prepare()
+        time.sleep(2)
+        transactions= self.rpc.wallet.transactions("a", 0,1, False)
+        assert len(transactions)==1
+        tx= transactions[0]
+        txid= tx["transaction_id"]
+        print("txid={}".format(txid))
+        print(transactions)
+        self.rpc.staking.deposit(self.node1_address1, [{'id':txid, 'index':0}], "a")
+        print("done")
+
 
     def read_info(self):
         print("read data")
@@ -177,7 +204,13 @@ class Program :
             data = json.load(json_file)
         print(json.dumps(data,indent=4))
         self.node0_address= data["nodes"][0]["staking"][0]
+        self.node0_address1= data["nodes"][0]["staking"][1]
+        self.node0_transfer_address= data["nodes"][0]["transfer"][0]
+
         self.node1_address= data["nodes"][1]["staking"][0]
+        self.node1_address1= data["nodes"][1]["staking"][1]
+        self.node1_transfer_address= data["nodes"][1]["transfer"][0]
+
         self.node2_address= data["nodes"][2]["staking"][0]
 
         self.node0_mnemonics=data["nodes"][0]["mnemonic"]
@@ -187,6 +220,8 @@ class Program :
     def display_info(self):
         print("jail test current hash={}".format(CURRENT_HASH))
         print("node0 staking= {}".format(self.node0_address))
+        print("node0 staking1= {}".format(self.node0_address1))
+        print("node0 transfer= {}".format(self.node0_transfer_address))
         print("node1 staking= {}".format(self.node1_address))
         print("node2 staking= {}".format(self.node2_address))
         print("node0 mnemonics= {}".format(self.node0_mnemonics))

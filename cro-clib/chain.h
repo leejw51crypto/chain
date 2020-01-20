7775f37f0342e8cbbf7999f0c4418d4eef8e3303
jongwhan lee <jonghwan@crypto.com>
2020-01-20 15:52:57 +0800

    Problem:(cro-819) no C bindings for client libraries
    Solution: add c-bindings
    remove auto-generated files
    
    working
    
    tidy up
    
    fix clippy issues
    
    remove chain.h
    
    
    tidy up
    
    
    tidy up
    
    
    tidy up
    
    
    tidy up lines
    
    
    refactoring api design
    
    
    tidy up
    
    user provice mnemonics
    
    update chain.h
    
    function call works fine
    
    change to copy
    
    
    add example
    
    
    tidy up
    
    
    check restore
    
    show extended address
    
    tidy up
    
    add remove address
    
    
    tidy up
    
    tidy up
    
    
    Problem:(cro-819) no C bindings for client libraries
    
    Solution: add c-bindings
    remove auto-generated files
    
    working
    
    tidy up
    
    fix clippy issues
    
    remove chain.h
    
    
    tidy up
    
    
    tidy up
    
    
    tidy up
    
    
    tidy up lines
    
    
    refactoring api design
    
    
    tidy up
    
    user provice mnemonics
    
    update chain.h
    
    function call works fine
    
    change to copy
    
    
    tidy up
    
    tidy up
    
    
    add viewkey
    
    show viewkey correctly
    
    
    add assert
    
    
    add unit test
    
    
    change fail const
    
    
    add prefix
    
    
    add Cro prefix directly
    
    
    add get raw address
    
    
    fix clippy
    
    
    add null check
    
    
    change buf to address

---
+++ cro-clib/chain.h
@@ -0,0 +1,102 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define FAIL -1
+
+#define SUCCESS 0
+
+/**
+ * network kinds
+ */
+typedef enum CroNetwork {
+  Mainnet,
+  Testnet,
+  Devnet,
+} CroNetwork;
+
+typedef struct CroAddress CroAddress;
+
+typedef struct CroHDWallet CroHDWallet;
+
+typedef struct CroResult {
+  int result;
+} CroResult;
+
+typedef CroHDWallet *CroHDWalletPtr;
+
+typedef CroAddress *CroAddressPtr;
+
+/**
+ * # Safety
+ */
+CroResult cro_create_hdwallet(CroHDWalletPtr *wallet_out,
+                              uint8_t *mnemonics,
+                              uint32_t mnemonics_length);
+
+/**
+ * create staking address from bip44 hdwallet
+ * # Safety
+ */
+CroResult cro_create_staking_address(CroHDWalletPtr wallet_ptr,
+                                     CroNetwork network,
+                                     CroAddressPtr *address_out,
+                                     uint32_t index);
+
+/**
+ * create utxo address from bip44 wallet, which is for withdrawal, transfer amount
+ * # Safety
+ */
+CroResult cro_create_transfer_address(CroHDWalletPtr wallet_ptr,
+                                      CroNetwork network,
+                                      CroAddressPtr *address_out,
+                                      uint32_t index);
+
+/**
+ * create viewkey, which is for encrypted tx
+ * # Safety
+ */
+CroResult cro_create_viewkey(CroHDWalletPtr wallet_ptr,
+                             CroNetwork network,
+                             CroAddressPtr *address_out,
+                             uint32_t index);
+
+/**
+ * destroy address
+ * # Safety
+ */
+CroResult cro_destroy_address(CroAddressPtr addr);
+
+/**
+ * destroy bip44 hdwallet
+ * # Safety
+ */
+CroResult cro_destroy_hdwallet(CroHDWalletPtr hdwallet);
+
+/**
+ * print address information
+ * # Safety
+ */
+CroResult cro_get_printed_address(CroAddressPtr address_ptr,
+                                  uint8_t *address_output,
+                                  uint32_t address_output_length);
+
+/**
+ * print address information
+ * # Safety
+ */
+CroResult cro_get_raw_address(CroAddressPtr address_ptr,
+                              uint8_t *address_output,
+                              uint32_t *address_output_length);
+
+/**
+ * print address information
+ * # Safety
+ */
+CroResult cro_print_address(CroAddressPtr address_ptr);
+
+/**
+ * # Safety
+ */
+CroResult cro_restore_hdwallet(const char *mnemonics_string, CroHDWalletPtr *wallet_out);

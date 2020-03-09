07b681eda55fd70faaa0dc676c8fa05f83d4dc7f
jongwhan lee <jonghwan@crypto.com>
2020-03-09 18:02:42 +0800

    add print

--- cro-clib/examples/IosSample/IosSample/ViewController.swift
+++ cro-clib/examples/IosSample/IosSample/ViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wallet_name: UITextField!
+    @IBOutlet weak var wallet_passphrase: UITextField!
+    @IBOutlet weak var wallet_mnemonics: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func click_create_wallet(_ sender: Any) {
-        print("click wallet")
+        var name = wallet_name.text!
+        var passphrase = wallet_passphrase.text!
+        var mnemonics = wallet_mnemonics.text!;
+        print("click wallet = \(name)  passphrase=\(passphrase) mnemonics=\(mnemonics	)")
     }
     
+    @IBAction func click_create_sync(_ sender: Any) {
+        print("click sync")
+    }
 }
 

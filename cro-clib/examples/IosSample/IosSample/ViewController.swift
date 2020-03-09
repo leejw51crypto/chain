9db5074f945f63bb066a8daf46812c5db5c348b2
jongwhan lee <jonghwan@crypto.com>
2020-03-09 17:13:35 +0800

    use storyboard

---
+++ cro-clib/examples/IosSample/IosSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosSample
+//
+//  Created by leejw51 on 9/3/2020.
+//  Copyright © 2020 leejw51. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

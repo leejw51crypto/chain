1299f3d88be2df9e479dec80cb70a84cc48ae929
jongwhan lee <jonghwan@crypto.com>
2020-03-23 11:47:23 +0800

    Problem: infeasible to create a large chunk of transfer addresses in HD wallet in client-*#1233
    Solution: make wallet to use storage directly
    tidy up
    
    
    tidy up client-rpc
    
    
    use string as key
    
    
    add batch
    
    
    rename stakingkey
    
    
    replace expect with ?

--- client-core/src/wallet/default_wallet_client.rs
+++ client-core/src/wallet/default_wallet_client.rs
@@ -351,7 +351,7 @@ where
         })?;
 
         // the passphrase is verified here.
-        let wallet = self.wallet_service.delete(name, &enckey)?;
+        self.wallet_service.delete(name, &enckey)?;
         self.sync_state_service.delete_global_state(name)?;
         self.wallet_state_service
             .delete_wallet_state(name, &enckey)?;
@@ -360,10 +360,6 @@ where
         }
         self.key_service.delete_wallet_private_key(name, &enckey)?;
 
-        for root_hash in wallet.root_hashes.iter() {
-            self.root_hash_service
-                .delete_root_hash(name, root_hash, &enckey)?;
-        }
         Ok(())
     }
 

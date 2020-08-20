42ca917b3b2e8644cd6872228033563c20a77b12
jongwhan lee <jonghwan@crypto.com>
2020-08-20 10:58:49 +0800

    tidy up tx-validation-server

--- chain-abci/src/enclave_bridge/edp/server.rs
+++ chain-abci/src/enclave_bridge/edp/server.rs
@@ -10,11 +10,10 @@ use chain_tx_validation::witness::verify_tx_recover_address;
 use chain_tx_validation::ChainInfo;
 use enclave_protocol::codec::StreamWrite;
 
-
 use enclave_protocol::IntraEnclaveRequest;
 use enclave_protocol::{
     EnclaveRequest, EnclaveResponse, IntraEnclaveResponseOk, IntraEncryptRequest,
-    ENCRYPTION_REQUEST_SIZE,QueryEncryptRequest
+    QueryEncryptRequest, ENCRYPTION_REQUEST_SIZE,
 };
 use parity_scale_codec::Decode;
 use std::io::Read;
@@ -73,63 +72,58 @@ impl<T: EnclaveProxy> TxValidationServer<T> {
     }
 
     fn encrypt_data(&mut self, req: Box<QueryEncryptRequest>) -> EnclaveResponse {
-        let result = {
-            let tx_inputs = match req.tx_inputs {
-                Some(inputs) => self.lookup_txids(inputs.iter().map(|x| x.id)),
-                _ => None,
-            };
-            match self.storage.get_last_app_state() {
-                Some(state) => {
-                    let last_state = ChainNodeState::decode(&mut state.as_slice())
-                        .expect("deserialize app state");
-                    let account = match req.op_sig {
-                        Some(sig) => self.lookup_state(
-                            &req.txid,
-                            &sig,
-                            last_state.staking_version,
-                        ),
-                        _ => None,
-                    };
-                    // TODO: fee in enclave?
+        let tx_inputs = match req.tx_inputs {
+            Some(inputs) => self.lookup_txids(inputs.iter().map(|x| x.id)),
+            _ => None,
+        };
+        let result = match self.storage.get_last_app_state() {
+            Some(state) => {
+                let last_state =
+                    ChainNodeState::decode(&mut state.as_slice()).expect("deserialize app state");
+                let account = match req.op_sig {
+                    Some(sig) => self.lookup_state(&req.txid, &sig, last_state.staking_version),
+                    _ => None,
+                };
+                // TODO: fee in enclave?
 
-                    let min_fee = last_state
-                        .top_level
-                        .network_params
-                        .calculate_fee(req.tx_size as usize)
-                        .expect("valid fee");
-                    let info = ChainInfo {
-                        min_fee_computed: min_fee,
-                        chain_hex_id: self.network_id,
-                        block_time: last_state.block_time,
-                        block_height: last_state.block_height,
-                        max_evidence_age: last_state.max_evidence_age,
-                    };
-                    let request = IntraEncryptRequest {
-                        txid: req.txid,
-                        sealed_enc_request: req.sealed_enc_request,
-                        tx_inputs,
-                        info,
-                        account,
-                    };
-                    // this is done in tx-validation
-                    let response = self.enclave.process_request(
-                        IntraEnclaveRequest::Encrypt(Box::new(request)),
-                    );
-                    match response {
-                        Ok(IntraEnclaveResponseOk::Encrypt(obftx)) => Ok(obftx),
-                        Ok(_) => {
-                            log::error!("unexpected response");
-                            Err(chain_tx_validation::Error::EnclaveRejected)
-                        }
-                        Err(e) => Err(e),
+                let min_fee = last_state
+                    .top_level
+                    .network_params
+                    .calculate_fee(req.tx_size as usize)
+                    .expect("valid fee");
+                let info = ChainInfo {
+                    min_fee_computed: min_fee,
+                    chain_hex_id: self.network_id,
+                    block_time: last_state.block_time,
+                    block_height: last_state.block_height,
+                    max_evidence_age: last_state.max_evidence_age,
+                };
+                let request = IntraEncryptRequest {
+                    txid: req.txid,
+                    sealed_enc_request: req.sealed_enc_request,
+                    tx_inputs,
+                    info,
+                    account,
+                };
+                // this is done in tx-validation
+                let response = self
+                    .enclave
+                    .process_request(IntraEnclaveRequest::Encrypt(Box::new(request)));
+                match response {
+                    Ok(IntraEnclaveResponseOk::Encrypt(obftx)) => Ok(obftx),
+                    Ok(_) => {
+                        log::error!("unexpected response");
+                        Err(chain_tx_validation::Error::EnclaveRejected)
                     }
-                }
-                None => {
-                    log::error!("can not find last app state");
-                    Err(chain_tx_validation::Error::EnclaveRejected)
+                    Err(e) => Err(e),
                 }
             }
+            None => {
+                log::error!("can not find last app state");
+                Err(chain_tx_validation::Error::EnclaveRejected)
+            }
         };
+
         EnclaveResponse::EncryptTx(result)
     }
     pub fn execute(&mut self) {
@@ -138,25 +132,27 @@ impl<T: EnclaveProxy> TxValidationServer<T> {
         let mut request = vec![0u8; ENCRYPTION_REQUEST_SIZE];
         loop {
             // this is request from tx-query -> abci
-            if let Ok(r_len) = self.socket_to_enclave.read(&mut request) {
-                log::debug!("received a message");
-                let mcmd = EnclaveRequest::decode(&mut request[..r_len].as_ref());
-                let resp = match mcmd {
-                    Ok(EnclaveRequest::GetSealedTxData { txids }) => {
-                        EnclaveResponse::GetSealedTxData(self.lookup_txids(txids.iter().copied()))
-                    }
-                    Ok(EnclaveRequest::EncryptTx(req)) => {
-                        self.encrypt_data(req)
-                    }
-                    Err(e) => {
-                        log::error!("unknown request / failed to decode: {}", e);
-                        EnclaveResponse::UnknownRequest
-                    }
-                };
-                // to sgx tx-query
-                if let Err(err) = resp.write_to(&mut self.socket_to_enclave) {
-                    log::error!("Error writing back tx-query response: {}", err);
+            let result = self.socket_to_enclave.read(&mut request);
+            if result.is_err() {
+                continue;
+            }
+            let r_len = result.expect("get r_len");
+
+            log::debug!("received a message");
+            let mcmd = EnclaveRequest::decode(&mut request[..r_len].as_ref());
+            let resp = match mcmd {
+                Ok(EnclaveRequest::GetSealedTxData { txids }) => {
+                    EnclaveResponse::GetSealedTxData(self.lookup_txids(txids.iter().copied()))
                 }
+                Ok(EnclaveRequest::EncryptTx(req)) => self.encrypt_data(req),
+                Err(e) => {
+                    log::error!("unknown request / failed to decode: {}", e);
+                    EnclaveResponse::UnknownRequest
+                }
+            };
+            // to sgx tx-query
+            if let Err(err) = resp.write_to(&mut self.socket_to_enclave) {
+                log::error!("Error writing back tx-query response: {}", err);
             }
         }
     }
